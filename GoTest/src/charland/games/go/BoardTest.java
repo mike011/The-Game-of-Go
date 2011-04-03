3181389c76c020fb24642ef321c9c54ed460ce55
Michael Charland <mike_ch_1@hotmail.com>
2011-04-02 22:00:42 -0400

    Adding tests

---
+++ GoTest/src/charland/games/go/BoardTest.java
@@ -0,0 +1,218 @@
+/**
+ * 
+ */
+package charland.games.go;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+/**
+ * @author Michael
+ * 
+ */
+public class BoardTest extends TestCase {
+
+    /**
+     * Make sure everything is empty to start.
+     */
+    public void testIsOccupiedStart() {
+        Board b = new Board();
+        for (int x = 0; x < 9; x++) {
+            for (int y = 0; y < 9; y++) {
+                Assert.assertEquals("Cell x:" + x + " y:" + y + " Should not be occupied", Board.EMPTY, b.isOccupied(x, y));
+            }
+        }
+    }
+
+    /**
+     * Occupy a cell with black.
+     */
+    public void testIsOccupiedBlack() {
+        Board b = new Board();
+        b.occupyBlack(4, 3);
+        Assert.assertEquals("Should be occupied", Board.BLACK, b.isOccupied(4, 3));
+    }
+
+    /**
+     * Occupy a cell with white.
+     */
+    public void testIsOccupiedWhite() {
+        Board b = new Board();
+        b.occupyWhite(2, 7);
+        Assert.assertEquals("Should be occupied", Board.WHITE, b.isOccupied(2, 7));
+    }
+
+    /**
+     * Tests the short[] constructor to make sure the parsing is working as expected.
+     */
+    public void testBoardShortArrayConstructor() {
+
+        short[] array = new short[Board.SIZE * Board.SIZE];
+        for (int x = 0; x < Board.SIZE * Board.SIZE; x++) {
+            array[x] = Board.EMPTY;
+        }
+
+        Board b = new Board(array);
+        for (int x = 0; x < Board.SIZE; x++) {
+            for (int y = 0; y < Board.SIZE; y++) {
+                Assert.assertEquals("Cell x:" + x + " y:" + y + " Should not be occupied", Board.EMPTY, b.isOccupied(x, y));
+            }
+        }
+    }
+
+    /**
+     * Tests that you can convert from a double array to a single array.
+     */
+    public void testGetShortArray() {
+        Board b = new Board();
+        short[] shortArray = b.getShortArray();
+        for (int y = 0; y < Board.SIZE * Board.SIZE; y++) {
+            Assert.assertEquals("Cell " + y + " should not be occupied", Board.EMPTY, shortArray[y]);
+        }
+    }
+
+    /**
+     * Test an empty board which shouldn't have any cells occupied.
+     */
+    public void testLibertiesCheck() {
+        Board b = new Board();
+        Assert.assertEquals("Cell should be onoccupied", -1, b.checkLiberties(0, 0, Board.BLACK));
+    }
+
+    /**
+     * Test a spot which should have the max amount of liberties.
+     */
+    public void testLibertiesCheck_Four() {
+        Board b = new Board();
+        int x = 1, y = 1;
+        b.occupyBlack(x, y);
+        Assert.assertEquals("Amount of liberties wrong", 4, b.checkLiberties(x, y, Board.BLACK));
+    }
+
+    /**
+     * Test a spot which should have the max amount of liberties and a stone to the left adding those liberties to the total.
+     */
+    public void testLibertiesCheck_Multi_LeftStone() {
+        Board b = new Board();
+        int x = 2, y = 1;
+        b.occupyBlack(x - 1, y);
+        b.occupyBlack(x, y);
+        Assert.assertEquals("Amount of liberties wrong", 6, b.checkLiberties(x, y, Board.BLACK));
+    }
+
+    /**
+     * Test a spot which should have the max amount of liberties and a stone to the right adding those liberties to the
+     * total.
+     */
+    public void testLibertiesCheck_Multi_RightStone() {
+        Board b = new Board();
+        int x = 1, y = 1;
+        b.occupyBlack(x, y);
+        b.occupyBlack(x + 1, y);
+        Assert.assertEquals("Amount of liberties wrong", 6, b.checkLiberties(x, y, Board.BLACK));
+    }
+
+    /**
+     * Test a spot which should have the max amount of liberties and a stone to the below adding those liberties to the
+     * total.
+     */
+    public void testLibertiesCheck_Multi_DownStone() {
+        Board b = new Board();
+        int x = 1, y = 1;
+        b.occupyBlack(x, y);
+        b.occupyBlack(x, y + 1);
+        Assert.assertEquals("Amount of liberties wrong", 6, b.checkLiberties(x, y, Board.BLACK));
+    }
+
+    /**
+     * Test a spot which should have the max amount of liberties and a stone to the above adding those liberties to the
+     * total.
+     */
+    public void testLibertiesCheck_Multi_UpStone() {
+        Board b = new Board();
+        int x = 1, y = 2;
+        b.occupyBlack(x, y - 1);
+        b.occupyBlack(x, y);
+        Assert.assertEquals("Amount of liberties wrong", 6, b.checkLiberties(x, y, Board.BLACK));
+    }
+
+    /**
+     * Test the top left corner.
+     */
+    public void testLibertiesCheck_TopLeft() {
+        Board b = new Board();
+        int x = 0, y = 0;
+        b.occupyBlack(x, y);
+        Assert.assertEquals("Amount of liberties wrong", 2, b.checkLiberties(x, y, Board.BLACK));
+    }
+
+    /**
+     * Test the top side.
+     */
+    public void testLibertiesCheck_TopSide() {
+        Board b = new Board();
+        int x = 5, y = 0;
+        b.occupyBlack(x, y);
+        Assert.assertEquals("Amount of liberties wrong", 3, b.checkLiberties(x, y, Board.BLACK));
+    }
+
+    /**
+     * Test the left side.
+     */
+    public void testLibertiesCheck_LeftSide() {
+        Board b = new Board();
+        int x = 0, y = 3;
+        b.occupyBlack(x, y);
+        Assert.assertEquals("Amount of liberties wrong", 3, b.checkLiberties(x, y, Board.BLACK));
+    }
+
+    /**
+     * Test the top right corner.
+     */
+    public void testLibertiesCheck_TopRight() {
+        Board b = new Board();
+        int x = Board.SIZE - 1, y = 0;
+        b.occupyBlack(x, y);
+        Assert.assertEquals("Amount of liberties wrong", 2, b.checkLiberties(x, y, Board.BLACK));
+    }
+
+    /**
+     * Test the right side.
+     */
+    public void testLibertiesCheck_RightSide() {
+        Board b = new Board();
+        int x = Board.SIZE - 1, y = 6;
+        b.occupyBlack(x, y);
+        Assert.assertEquals("Amount of liberties wrong", 3, b.checkLiberties(x, y, Board.BLACK));
+    }
+
+    /**
+     * Test the bottom left corner.
+     */
+    public void testLibertiesCheck_BottomLeft() {
+        Board b = new Board();
+        int x = 0, y = Board.SIZE - 1;
+        b.occupyBlack(x, y);
+        Assert.assertEquals("Amount of liberties wrong", 2, b.checkLiberties(x, y, Board.BLACK));
+    }
+
+    /**
+     * Test the bottom side.
+     */
+    public void testLibertiesCheck_BottomSide() {
+        Board b = new Board();
+        int x = 2, y = Board.SIZE - 1;
+        b.occupyBlack(x, y);
+        Assert.assertEquals("Amount of liberties wrong", 3, b.checkLiberties(x, y, Board.BLACK));
+    }
+
+    /**
+     * Test the bottom right corner.
+     */
+    public void testLibertiesCheck_BottomRight() {
+        Board b = new Board();
+        int x = Board.SIZE - 1, y = Board.SIZE - 1;
+        b.occupyBlack(x, y);
+        Assert.assertEquals("Amount of liberties wrong", 2, b.checkLiberties(x, y, Board.BLACK));
+    }
+}
